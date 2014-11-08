eb21a71d6f2789ab6108e4391e0d9dcde43b9020
T Wallace <wallacet@email.arizona.edu>
2014-11-08 11:21:26 -0700

    Projectile script added.

--- Assets/Scripts/PickUpScript.cs
+++ Assets/Scripts/PickUpScript.cs
@@ -7,7 +7,7 @@ public class PickUpScript : MonoBehaviour {
 	public void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Player") {
 			other.gameObject.GetComponent<PlayerWeaponScript>().EquipWeapon(gameObject.GetComponent<WeaponScript>());
-			this.enabled = false;
+			Destroy(gameObject);
 		}
 	}
 }
