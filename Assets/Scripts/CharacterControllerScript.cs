23acd45a09b55d8f712ad38b724621b1694e60d0
dmb1 <dmb1@email.arizona.edu>
2014-11-08 10:16:12 -0700

    changes to make character controller script time dependent.

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CharacterControllerScript : MonoBehaviour {
 	SpriteAnimationManagerScript manager;
+	public float speed = 2.0f;
 	// Use this for initialization
 	void Start () {
 		manager = GetComponent<SpriteAnimationManagerScript> ();
@@ -11,14 +12,14 @@ public class CharacterControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButton ("Right")) {
-			this.gameObject.transform.position = new Vector2 ((transform.position.x + 1), transform.position.y);
+			this.gameObject.transform.position = new Vector2 ((transform.position.x + speed * Time.deltaTime), transform.position.y);
 			manager.PlayAnimation("WalkRight");
 		} 
 		else if (Input.GetButton ("Left")) {
-			this.gameObject.transform.position = new Vector2 ((transform.position.x - 1), transform.position.y);
+			this.gameObject.transform.position = new Vector2 ((transform.position.x - speed * Time.deltaTime), transform.position.y);
 		} 
 		else if (Input.GetButton ("Jump")) {
-				this.gameObject.transform.position = new Vector2 ((transform.position.x), transform.position.y + 1);
+			this.gameObject.transform.position = new Vector2 ((transform.position.x), transform.position.y + speed * Time.deltaTime);
 		}
 	}
 }
