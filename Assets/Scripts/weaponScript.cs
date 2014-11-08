28377581cf1d602662e0a4b08a047d3cf876446a
Patrick Wilkening <wilkeningp@gmail.com>
2014-11-07 23:46:01 -0700

    made a weapon script

---
+++ Assets/Scripts/weaponScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weaponScript : MonoBehaviour {
+
+	// Use this for initialization
+	protected void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+	
+	}
+
+	public virtual void Use() {
+
+	}
+}
