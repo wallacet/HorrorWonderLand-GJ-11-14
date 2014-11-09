b997425a98b4b09edbd3d81b22d911d0c197b11c
T Wallace <wallacet@email.arizona.edu>
2014-11-08 20:30:12 -0700

    Organize sprites.

--- Assets/Scripts/FloatingWeaponScript.cs
+++ Assets/Scripts/FloatingWeaponScript.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class FloatingWeaponScript : MonoBehaviour {
 
+	public float amplitude = 0.2f;
+	public float period = 1;
+	private float originalY;
+
+	void Start() {
+		originalY = transform.position.y;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = new Vector3(transform.position.x, originalY + amplitude * Mathf.Sin(period * Time.realtimeSinceStartup), 0);
 	}
 }
--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -4,22 +4,31 @@ using System.Collections;
 [RequireComponent(typeof(SpriteAnimationManagerScript))]
 public class HealthScript : MonoBehaviour {
 	public float health = 10.0f;
+	public bool dead = false;
 
 	public void Damage(float damage) {
 		health -= damage;
 	}
 
-	public virtual void Die() {
-		if(LayerMask.NameToLayer("Enemy") == gameObject.layer)
+	public virtual void Die() { 
+		if(LayerMask.NameToLayer("Enemy") == gameObject.layer) {
 			GameObject.Find("Main Character").GetComponent<EnemyCounterScript>().enemyCount--;
+			GetComponent<AiScript>().enabled = false;
+		}
+		else {
+			foreach(BoxCollider2D box in gameObject.GetComponents<BoxCollider2D>())
+				box.enabled=false;
+		}
+
 		gameObject.GetComponent<SpriteAnimationManagerScript>().PlayAnimation("Death");
-		foreach(BoxCollider2D box in gameObject.GetComponents<BoxCollider2D>())
-			box.enabled=false;
+		gameObject.layer = LayerMask.NameToLayer("Dead");
 	}
 	
 	// Update is called once per frame
 	public void Update () {
-		if(health <= 0)
+		if(health <= 0 && !dead) {
 			Die();
+			dead = true;
+		}
 	}
 }
