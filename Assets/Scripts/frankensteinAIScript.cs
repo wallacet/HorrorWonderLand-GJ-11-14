eb5e0322dfa06b3c1047f138be6ef2cc639ca876
Patrick Wilkening <wilkeningp@gmail.com>
2014-11-08 21:07:36 -0700

    implemented frankenstein AI

--- Assets/Scripts/frankensteinAIScript.cs
+++ Assets/Scripts/frankensteinAIScript.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class FrankensteinAIScript : AiScript {
 
+	private GameObject player;
+	private SpriteAnimationManagerScript sams;
+
 	// Use this for initialization
 	public override void Start () {
 		base.Start ();
+		player = GameObject.Find ("Player");
+		sams = gameObject.GetComponent<SpriteAnimationManagerScript>();
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
 		base.Update ();
+		Alerted ();
+	}
+
+	public override void Move ()
+	{
+		if(player.transform.position.x > gameObject.transform.position.x) {
+			transform.position += new Vector3(speed * Time.deltaTime,0,0);
+			sams.PlayAnimation("WalkRight");
+		}
+		else {
+			transform.position -= new Vector3(speed * Time.deltaTime,0,0);
+			sams.PlayAnimation("WalkLeft");
+		}
+	}
+
+	public override void Alerted ()
+	{
+		base.Alerted ();
+		if (isTriggered) 
+		{
+			this.Move();
+		}
 	}
 }
