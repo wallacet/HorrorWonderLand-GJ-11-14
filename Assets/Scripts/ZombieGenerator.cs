91f29f19d07f01eeb591fba1bd69281dac308b1d
dmb1 <dmb1@email.arizona.edu>
2014-11-09 11:09:43 -0700

    second scene and zombie generator.

---
+++ Assets/Scripts/ZombieGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieGenerator : MonoBehaviour {
+
+	public GameObject Zombie;
+	private float currentTime;
+	private bool zombieSpawned;
+	// Use this for initialization
+	void Start () {
+		currentTime = Time.realtimeSinceStartup;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (((int)(Time.realtimeSinceStartup - currentTime) % 10) == 0) {
+						if (!zombieSpawned) {
+								Instantiate (Zombie, transform.position, Quaternion.identity);
+								zombieSpawned = true;
+						}
+				} else {
+			zombieSpawned = false;
+				}
+	}
+}
